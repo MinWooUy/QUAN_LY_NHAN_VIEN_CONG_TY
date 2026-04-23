8134161a12b5719679a0f4923984b546bbf8d6e9
Nguyễn Thanh Huy <146328871+huy489@users.noreply.github.com>
2026-04-23 16:33:26 +0700

    Update DonXinNghi.h

--- src/DonXinNghi.h
+++ src/DonXinNghi.h
@@ -17,8 +17,38 @@ private:
 
 public:
     DonXinNghi();
+
+    DonXinNghi(
+        const string& maDonNghi,
+        const string& maNhanVien,
+        const string& loaiNghi,
+        const string& ngayBatDau,
+        const string& ngayKetThuc,
+        const string& lyDo,
+        const string& trangThaiDuyet,
+        const string& nguoiDuyet
+    );
+
     void nhapThongTin();
-    void hienThiThongTin();
+    void hienThiThongTin() const;
+    void capNhatThongTin();
+
+    string getMaDonNghi() const;
+    string getMaNhanVien() const;
+    string getLoaiNghi() const;
+    string getNgayBatDau() const;
+    string getNgayKetThuc() const;
+    string getLyDo() const;
+    string getTrangThaiDuyet() const;
+    string getNguoiDuyet() const;
+
+    void setMaNhanVien(const string& maNhanVien);
+    void setLoaiNghi(const string& loaiNghi);
+    void setNgayBatDau(const string& ngayBatDau);
+    void setNgayKetThuc(const string& ngayKetThuc);
+    void setLyDo(const string& lyDo);
+    void setTrangThaiDuyet(const string& trangThaiDuyet);
+    void setNguoiDuyet(const string& nguoiDuyet);
 };
 
 #endif
